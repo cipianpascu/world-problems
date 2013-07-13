039eebefc0f452aa047ef207d779e552d995d063
Ciprian Pascu <ciprianpascu@yahoo.com>
2013-07-13 11:04:59 +0300

    Added .gitignore file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.write(fs.readFileSync('./index.html'));
+  response.end();
 });
 
 var port = process.env.PORT || 5000;
